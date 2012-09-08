c55262a7626e2878c9c1a5e6dc23e6cf7db67814
Shota Fukumori (sora_h) <sorah@tubusu.net>
2012-09-08 12:18:29 +0900

    DQXTools.status and up?

--- lib/dqx_tools.rb
+++ lib/dqx_tools.rb
@@ -1,5 +1,16 @@
-require "dqx_tools/version"
+require_relative "./dqx_tools/version"
+require_relative "./dqx_tools/util"
 
-module DqxTools
-  # Your code goes here...
+module DQXTools
+  class << self
+    def status
+      page = Util.get_page("http://hiroba.dqx.jp/sc/")
+      return :maintenance if page.at("#serverStatusLabelMainte")
+      return :running
+    end
+
+    def up?
+      self.status == :running
+    end
+  end
 end
